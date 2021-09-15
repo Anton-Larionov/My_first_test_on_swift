f04c28a5ed712e63c65ad6ada2217778fd4275a2
Antony-py <dssmedia@yandex.ru>
2021-09-15 11:36:05 +0300

    my first_sintax on swift

---
+++ Contents.swift
@@ -0,0 +1,51 @@
+import UIKit
+
+var str = "Hello, playground"
+
+func sum(a: Int, b: Int) {
+    print(a + b)
+}
+
+sum(a: 1, b: 1)
+
+func sumOptinal(a: Int? = nil) {
+    if let aNotOptional = a {
+        print(aNotOptional)
+    }
+}
+
+sumOptinal(a: 345)
+
+struct User {
+    var name, address: String
+    var age: Int
+    
+}
+
+var arrayUsers = [User]()
+arrayUsers.append(User(name: "Sergey", address: "Moscow", age: 20))
+arrayUsers.append(User(name: "Vasya", address: "Tver", age: 16))
+
+print(arrayUsers.count)
+
+for user in arrayUsers {
+    print(user.name)
+}
+
+class UserClass {
+    var arrayUsers = [User]()
+    init() {
+        setUsers()
+    }
+    private func setUsers() {
+        self.arrayUsers.append(User(name: "Sergey", address: "Moscow", age: 20))
+        self.arrayUsers.append(User(name: "Vasya", address: "Tver", age: 16))
+    }
+    func getAddreses() {
+        for user in self.arrayUsers {
+            print(user.address)
+        }
+    }
+}
+let users = UserClass()
+users.getAddreses()
